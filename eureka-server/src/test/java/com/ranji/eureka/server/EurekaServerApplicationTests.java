5048b924be7174c4f4ae040eb55c54f7d2e73cc8
rhoshino <rhoshino@insside.net>
2021-07-02 14:53:56 -0300

    enable eureka service

---
+++ eureka-server/src/test/java/com/ranji/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranji.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
